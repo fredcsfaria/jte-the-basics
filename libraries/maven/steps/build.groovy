2485e5ef31fc9dc6228ab0ffc4c3743e5a356d0a
fredcsfaria <52935549+fredcsfaria@users.noreply.github.com>
2024-02-22 16:32:15 -0300

    Update build.groovy

--- libraries/maven/steps/build.groovy
+++ libraries/maven/steps/build.groovy
@@ -4,6 +4,6 @@ void call(){
          println "Prepara para build"
        // seta contexto default - build Host
      //  sh 'docker context use default'
-       checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[cancelProcessOnExternalsFail: true, credentialsId: 'svn_repositorio', depthOption: 'infinity', ignoreExternalsOption: true, local: '.', remote: "${config.branchName}" ]], quietOperation: true, workspaceUpdater: [$class: 'UpdateUpdater']])
+       checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[cancelProcessOnExternalsFail: true, credentialsId: 'svn_repositorio', depthOption: 'infinity', ignoreExternalsOption: true, local: '.', remote: "https://cd-build-01:8083/svn/gsi/ws-sgd/branches/migracao_container" ]], quietOperation: true, workspaceUpdater: [$class: 'UpdateUpdater']])
     }
 }
