c7e0956f6f6a00dd59489aea53d06df678663373
fredcsfaria <52935549+fredcsfaria@users.noreply.github.com>
2024-02-22 16:24:53 -0300

    Update build.groovy

--- libraries/maven/steps/build.groovy
+++ libraries/maven/steps/build.groovy
@@ -1,9 +1,9 @@
 void call(){
     stage("Maven: Build"){
-        println "build from the maven library ${config.branchName} "
+        println "build from the maven library ${config.branchName}       "
          println "Prepara para build"
        // seta contexto default - build Host
      //  sh 'docker context use default'
-       checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[cancelProcessOnExternalsFail: true, credentialsId: 'svn_repositorio', depthOption: 'infinity', ignoreExternalsOption: true, local: '.', remote: "${config.SVN_REPO_URL}"]], quietOperation: true, workspaceUpdater: [$class: 'UpdateUpdater']])
+       checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[cancelProcessOnExternalsFail: true, credentialsId: 'svn_repositorio', depthOption: 'infinity', ignoreExternalsOption: true, local: '.', remote: "${config.branchName}" ]], quietOperation: true, workspaceUpdater: [$class: 'UpdateUpdater']])
     }
 }
