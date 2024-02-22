132c3e82733e532421ea19ee9be316877c979742
fredcsfaria <52935549+fredcsfaria@users.noreply.github.com>
2024-02-22 15:14:27 -0300

    Update prepare.groovy

--- libraries/mvc-j/steps/prepare.groovy
+++ libraries/mvc-j/steps/prepare.groovy
@@ -1,4 +1,11 @@
-  // seta contexto default - build Host
-  sh 'docker context use default'
-  checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[cancelProcessOnExternalsFail: true, credentialsId: 'svn_repositorio', depthOption: 'infinity', ignoreExternalsOption: true, local: '.', remote: "${SVN_REPO_URL}"]], quietOperation: true, workspaceUpdater: [$class: 'UpdateUpdater']])
-                
+void call(){
+    stage("mvc-j: Prepare"){
+        println "Prepara para build"
+       // seta contexto default - build Host
+       sh 'docker context use default'
+       checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[cancelProcessOnExternalsFail: true, credentialsId: 'svn_repositorio', depthOption: 'infinity', ignoreExternalsOption: true, local: '.', remote: "${SVN_REPO_URL}"]], quietOperation: true, workspaceUpdater: [$class: 'UpdateUpdater']])
+    }
+}
+
+
+
