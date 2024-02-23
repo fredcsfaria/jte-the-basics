807fce60e9e1ab10d5bf65143aec0350c2a7abe7
fredcsfaria <52935549+fredcsfaria@users.noreply.github.com>
2024-02-23 16:03:16 -0300

    Update prepare.groovy

--- libraries/mvc-j/steps/prepare.groovy
+++ libraries/mvc-j/steps/prepare.groovy
@@ -1,11 +1,11 @@
-void call(){
-    stage("mvc-j: Prepare"){
-        println "Prepara para build"
+void call( String name1){
+    stage("Maven: Build"){
+        println "build from the maven library   ${name1} " 
+        echo name1
+         println "Prepara para build"
        // seta contexto default - build Host
-       sh 'docker context use default'
-       checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[cancelProcessOnExternalsFail: true, credentialsId: 'svn_repositorio', depthOption: 'infinity', ignoreExternalsOption: true, local: '.', remote: "${SVN_REPO_URL}"]], quietOperation: true, workspaceUpdater: [$class: 'UpdateUpdater']])
+     //  sh 'docker context use default'
+       checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[cancelProcessOnExternalsFail: true, credentialsId: 'svn_repositorio', depthOption: 'infinity', ignoreExternalsOption: true, local: '.', remote: name1 ]], quietOperation: true, workspaceUpdater: [$class: 'UpdateUpdater']])
     }
 }
-
-
 
