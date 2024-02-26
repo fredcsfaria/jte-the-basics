d517f2206548459e96bda4c992c17c81d0ef3760
fredcsfaria <52935549+fredcsfaria@users.noreply.github.com>
2024-02-26 17:25:48 -0300

    Update scanning.groovy

--- libraries/mvcj/steps/scanning.groovy
+++ libraries/mvcj/steps/scanning.groovy
@@ -1,6 +1,6 @@
-void call( String cimage){
+void call( String name1){
     stage("mvcj: Scanning"){
-        println "Scanning image  ${cimage} " 
+        println "Scanning image  ${name1} " 
         println "\""FRED\"""
       // sh  "trivy image   --vuln-type "os,library"  --scanners "vuln,secret"  --severity HIGH,CRITICAL  --format sarif --output trivy_result_sarif.ci.out  --exit-code "0"  --timeout 15m --ignore-unfixed --quiet ${cimage} "
                 
