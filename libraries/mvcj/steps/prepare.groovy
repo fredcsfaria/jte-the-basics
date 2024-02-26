aea945e57cea4aec5dc1430262071786600632de
fredcsfaria <52935549+fredcsfaria@users.noreply.github.com>
2024-02-26 14:22:39 -0300

    Update prepare.groovy

--- libraries/mvcj/steps/prepare.groovy
+++ libraries/mvcj/steps/prepare.groovy
@@ -1,10 +1,10 @@
-void call( String name1){
+void call( String branchName){
     stage("mvcj: Prepare"){
-        println "build from the maven library   ${name1} " 
+        println "Checkout from branch   ${branchName} " 
         echo name1
          println "Prepara para build"
        // seta contexto default - build Host
-     //  sh 'docker context use default'
+       sh 'docker context use default'
        checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[cancelProcessOnExternalsFail: true, credentialsId: 'svn_repositorio', depthOption: 'infinity', ignoreExternalsOption: true, local: '.', remote: name1 ]], quietOperation: true, workspaceUpdater: [$class: 'UpdateUpdater']])
     }
 }
