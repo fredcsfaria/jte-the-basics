72488e239caafd00b2cafece068ced1dff6d64e9
fredcsfaria <52935549+fredcsfaria@users.noreply.github.com>
2024-02-26 16:00:19 -0300

    Update container_image.groovy

--- libraries/mvcj/steps/container_image.groovy
+++ libraries/mvcj/steps/container_image.groovy
@@ -2,9 +2,9 @@ void call( app_env ){
     stage("mvcj: Deploy to "){
           println  "${app_env.long_name}"
         println "Build image from source " 
-          // withDockerRegistry(credentialsId: 'nexus_repository', url: "${URL_CONTAINER_REGISTRY}" ) {
-            //  sh 'skaffold build -f ./.skaffold.yaml -p ${SKAFFOLD_PROFILE}     --default-repo=${DEFAULT_REPOSITORY}  --file-output=skaffold_build_result_json.ci.out   --insecure-registry=${CONTAINER_REGISTRY}    --push=false  --tag=${TAG}'
-              //  }
+           withDockerRegistry(credentialsId: 'nexus_repository', url: "${app_env.URL_CONTAINER_REGISTRY}" ) {
+              sh 'skaffold build -f ./.skaffold.yaml -p ${app_env.SKAFFOLD_PROFILE}     --default-repo=${app_env.DEFAULT_REPOSITORY}  --file-output=skaffold_build_result_json.ci.out   --insecure-registry=${app_env.CONTAINER_REGISTRY}    --push=false  --tag=${config.TAG}'
+                }
         
     }
 }
