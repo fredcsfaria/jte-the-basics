b9392584416b47d0cb100661abb92a1e922c5e36
fredcsfaria <52935549+fredcsfaria@users.noreply.github.com>
2024-02-26 14:41:45 -0300

    Update and rename buildn.groovy to buildmaven.groovy

---
+++ libraries/mvcj/steps/buildmaven.groovy
@@ -0,0 +1,10 @@
+void call( String branchName){
+    stage("mvcj: Prepare"){
+        println "Checkout from branch   ${branchName} " 
+        
+         println "Prepara para build"
+       // seta contexto default - build Host
+       sh 'docker context use default'
+       checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[cancelProcessOnExternalsFail: true, credentialsId: 'svn_repositorio', depthOption: 'infinity', ignoreExternalsOption: true, local: '.', remote: branchName ]], quietOperation: true, workspaceUpdater: [$class: 'UpdateUpdater']])
+    }
+} 
