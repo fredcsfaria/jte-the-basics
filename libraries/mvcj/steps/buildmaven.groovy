9c18acdd7cf07235fd972cb42770cf3fb4b608e8
fredcsfaria <52935549+fredcsfaria@users.noreply.github.com>
2024-02-26 14:46:43 -0300

    Update buildmaven.groovy

--- libraries/mvcj/steps/buildmaven.groovy
+++ libraries/mvcj/steps/buildmaven.groovy
@@ -1,10 +1,9 @@
 void call( String branchName){
     stage("mvcj: Prepare"){
-        println "Checkout from branch   ${branchName} " 
+        println "Build com Maven - branch  ${branchName} " 
         
-         println "Prepara para build"
+         //println "Prepara para build"
        // seta contexto default - build Host
-       sh 'docker context use default'
-       checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[cancelProcessOnExternalsFail: true, credentialsId: 'svn_repositorio', depthOption: 'infinity', ignoreExternalsOption: true, local: '.', remote: branchName ]], quietOperation: true, workspaceUpdater: [$class: 'UpdateUpdater']])
+        sh ' mvn --batch-mode install -P nexus'
     }
 } 
