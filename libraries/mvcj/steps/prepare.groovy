97b36e39c7a7bb89886e94e850016d1bab8a8718
fredcsfaria <52935549+fredcsfaria@users.noreply.github.com>
2024-02-26 15:24:07 -0300

    Update prepare.groovy

--- libraries/mvcj/steps/prepare.groovy
+++ libraries/mvcj/steps/prepare.groovy
@@ -3,6 +3,7 @@ void call( String branchName){
         println "Checkout from branch   ${branchName} "
          println "Checkout from config branch ${config.branchName3}"    
          println "Checkout from keyword branch ${branchName1}" 
+          println "Checkout from keyword2 branch ${SVN_REPO_URL1}" 
           println "Checkout from config branch ${config.SVN_REPO_URL} " 
            println "Image to registry ${config.CONTAINER_REGISTRY} " 
          println "Prepara para build"
