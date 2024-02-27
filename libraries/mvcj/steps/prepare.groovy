14980b03d575f2df17cb764c0f964b960dfb2c3c
fredcsfaria <52935549+fredcsfaria@users.noreply.github.com>
2024-02-27 16:12:26 -0300

    Update prepare.groovy

--- libraries/mvcj/steps/prepare.groovy
+++ libraries/mvcj/steps/prepare.groovy
@@ -1,11 +1,11 @@
 void call( String branchName){
     stage("mvcj: Prepare"){
         println "Checkout from branch   ${branchName} "
-         println "Checkout from config branch ${config.branchName3}"    
-         println "Checkout from keyword branch ${branchName1}" 
-          println "Checkout from keyword2 branch ${SVN_REPO_URL1}" 
-          println "Checkout from config branch ${config.SVN_REPO_URL} " 
-           println "Image to registry ${config.CONTAINER_REGISTRY} " 
+       //  println "Checkout from config branch ${config.branchName3}"    
+       //  println "Checkout from keyword branch ${branchName1}" 
+        //  println "Checkout from keyword2 branch ${SVN_REPO_URL1}" 
+        //  println "Checkout from config branch ${config.SVN_REPO_URL} " 
+         //  println "Image to registry ${config.CONTAINER_REGISTRY} " 
          println "Prepara para build"
        // seta contexto default - build Host
        sh 'docker context use default'
