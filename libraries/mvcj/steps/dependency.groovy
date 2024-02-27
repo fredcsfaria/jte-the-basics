05a05645e0d48af0672fb9b79966b4fd2f2c81c7
fredcsfaria <52935549+fredcsfaria@users.noreply.github.com>
2024-02-27 12:13:58 -0300

    Update dependency.groovy

--- libraries/mvcj/steps/dependency.groovy
+++ libraries/mvcj/steps/dependency.groovy
@@ -1,7 +1,7 @@
 void call( String cimage){
     stage("mvcj: Dependency"){
         println "Produzindo informação de dependencias  ${cimage} " 
-        sh  ' trivy image --format cyclonedx  --scanners vuln --timeout 15m --output trivy-result-sbom-json.ci.out   ${cimage}  '
+        sh  " trivy image --format cyclonedx  --scanners vuln --timeout 15m --output trivy-result-sbom-json.ci.out   ${cimage}  "
         
         
     }
