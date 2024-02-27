d93c984f60b6d552fecc6be4e825c64a6a932fc5
fredcsfaria <52935549+fredcsfaria@users.noreply.github.com>
2024-02-27 12:19:58 -0300

    Update push.groovy

--- libraries/mvcj/steps/push.groovy
+++ libraries/mvcj/steps/push.groovy
@@ -1,12 +1,11 @@
-void call( String name1){
+void call( app_env){
     stage("mvcj: Dependency"){
         println "Push from the maven library   ${name1} " 
              withDockerRegistry(credentialsId: 'nexus_repository', url: "${URL_CONTAINER_REGISTRY}" ) {
-               
-          
-               sh 'skaffold build -f ./.skaffold.yaml  -p ${SKAFFOLD_PROFILE} --default-repo=${DEFAULT_REPOSITORY}   --file-output=build_result.json   --insecure-registry=${CONTAINER_REGISTRY}   --tag=${TAG} --push=true' 
-               
-                   }
+              withDockerRegistry(credentialsId: 'nexus_repository', url: "${app_env.URL_CONTAINER_REGISTRY}" ) {
+                  sh "skaffold build -f ./.skaffold.yaml  -p ${app_env.SKAFFOLD_PROFILE}  --default-repo=${app_env.DEFAULT_REPOSITORY}   --file-output=build_result.json   --insecure-registry=${app_env.CONTAINER_REGISTRY}   --tag=${TAG} --push=true "
+            
+              }
         
     }
 }
