fa0d552cefe15bf4a270071de300c25876f227ea
fredcsfaria <52935549+fredcsfaria@users.noreply.github.com>
2024-02-29 16:47:28 -0300

    Update deploy.groovy

--- libraries/mvcj/steps/deploy.groovy
+++ libraries/mvcj/steps/deploy.groovy
@@ -1,13 +1,12 @@
 void call( app_env ){
     stage("mvcj: Deploy to "){
          
-        println "Render artefatos para ${app_env.URL_CONTAINER_REGISTRY} ${IMAGE_NAME} " 
+        println "Render artefatos    " 
          script {
                  withKubeConfig(caCertificate: '',   credentialsId: "${app_env.CREDENCIAL_K8S}", namespace: 'default', restrictKubeConfigAccess: false, serverUrl: "${app_env.CLUSTER_K8S_API}") {
                 // some block
                 sh ' kubectl config view'
-             // sh 'skaffold apply -f ./.skaffold.yaml  -p ${SKAFFOLD_PROFILE}  k8s-deploy.yaml'
-                  
+                             
                         
                     }  
                  }
