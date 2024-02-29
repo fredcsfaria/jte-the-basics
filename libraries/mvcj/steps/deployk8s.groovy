5c70f9a69f8a68bae77ccdda3d650a33e8132827
fredcsfaria <52935549+fredcsfaria@users.noreply.github.com>
2024-02-29 16:49:01 -0300

    Create deployk8s.groovy

---
+++ libraries/mvcj/steps/deployk8s.groovy
@@ -0,0 +1,12 @@
+void call( app_env ){
+    stage("mvcj: Deploy "){
+         
+        println "Render artefatos para ${app_env.URL_CONTAINER_REGISTRY} ${IMAGE_NAME} " 
+           withDockerRegistry(credentialsId: 'nexus_repository', url: "${app_env.URL_CONTAINER_REGISTRY}" ) {
+              //sh "skaffold build -f ./.skaffold.yaml -p ${app_env.SKAFFOLD_PROFILE}     --default-repo=${app_env.DEFAULT_REPOSITORY}  --file-output=skaffold_build_result_json.ci.out   --insecure-registry=${app_env.CONTAINER_REGISTRY}    --push=false  --tag=${config.TAG}"
+             sh "skaffold render -f ./.skaffold.yaml  --digest-source=tag --default-repo=${app_env.DEFAULT_REPOSITORY}  --offline=true -p ${app_env.SKAFFOLD_PROFILE}  -o k8s-deploy.yaml -i app-image=${IMAGE_NAME} "
+               
+                }
+        
+    }
+}
