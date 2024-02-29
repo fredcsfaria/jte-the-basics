c535fa9675e2453c0cf723aa15581d3ef7d313dd
fredcsfaria <52935549+fredcsfaria@users.noreply.github.com>
2024-02-29 12:06:05 -0300

    Update render.groovy

--- libraries/mvcj-cd/steps/render.groovy
+++ libraries/mvcj-cd/steps/render.groovy
@@ -1,10 +1,10 @@
 void call( app_env ){
     stage("mvcj: Render to "){
          
-        println "Render artefatos " 
+        println "Render artefatos para ${IMAGE_NAME} " 
            withDockerRegistry(credentialsId: 'nexus_repository', url: "${app_env.URL_CONTAINER_REGISTRY}" ) {
               //sh "skaffold build -f ./.skaffold.yaml -p ${app_env.SKAFFOLD_PROFILE}     --default-repo=${app_env.DEFAULT_REPOSITORY}  --file-output=skaffold_build_result_json.ci.out   --insecure-registry=${app_env.CONTAINER_REGISTRY}    --push=false  --tag=${config.TAG}"
-              sh "skaffold render -f ./.skaffold.yaml  --digest-source=tag --default-repo=${app_env.DEFAULT_REPOSITORY}  --offline=true -p ${app_env.SKAFFOLD_PROFILE}  -o k8s-deploy.yaml -i app-image=${config.IMAGE_NAME} "
+              sh "skaffold render -f ./.skaffold.yaml  --digest-source=tag --default-repo=${app_env.DEFAULT_REPOSITORY}  --offline=true -p ${app_env.SKAFFOLD_PROFILE}  -o k8s-deploy.yaml -i app-image=${IMAGE_NAME} "
                 }
         
     }
