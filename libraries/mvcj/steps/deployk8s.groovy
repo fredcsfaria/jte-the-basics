db6cc88b9bad84ebbab93a0aaae4cf4071b5f21d
fredcsfaria <52935549+fredcsfaria@users.noreply.github.com>
2024-03-01 16:22:26 -0300

    Update deployk8s.groovy

--- libraries/mvcj/steps/deployk8s.groovy
+++ libraries/mvcj/steps/deployk8s.groovy
@@ -1,6 +1,7 @@
 void call( app_env ){
     stage("mvcj: Deploy "){
-         
+
+        println "${app_env.DEFAULT_REPOSITORY}" +   "${DEFAULT_REPOSITORY1}" + "${IMAGE_NAME1}"
         println "Render artefatos para ${app_env.URL_CONTAINER_REGISTRY} ${IMAGE_NAME} " 
           script {
                  withKubeConfig(caCertificate: '',   credentialsId: "${app_env.CREDENCIAL_K8S}", namespace: 'default', restrictKubeConfigAccess: false, serverUrl: "${app_env.CLUSTER_K8S_API}") {
@@ -10,11 +11,7 @@ void call( app_env ){
                       
                     }  
                  }
-        /*   withDockerRegistry(credentialsId: 'nexus_repository', url: "${app_env.URL_CONTAINER_REGISTRY}" ) {
-              //sh "skaffold build -f ./.skaffold.yaml -p ${app_env.SKAFFOLD_PROFILE}     --default-repo=${app_env.DEFAULT_REPOSITORY}  --file-output=skaffold_build_result_json.ci.out   --insecure-registry=${app_env.CONTAINER_REGISTRY}    --push=false  --tag=${config.TAG}"
-             sh "skaffold render -f ./.skaffold.yaml  --digest-source=tag --default-repo=${app_env.DEFAULT_REPOSITORY}  --offline=true -p ${app_env.SKAFFOLD_PROFILE}  -o k8s-deploy.yaml -i app-image=${IMAGE_NAME} "
-               
-                } */
+       
         
     }
 }
