c94f6c0ee0b618b58e9d5f33779f77bc7ebede0b
fredcsfaria <52935549+fredcsfaria@users.noreply.github.com>
2024-03-01 16:27:19 -0300

    Update deployk8s.groovy

--- libraries/mvcj/steps/deployk8s.groovy
+++ libraries/mvcj/steps/deployk8s.groovy
@@ -1,8 +1,8 @@
 void call( app_env ){
     stage("mvcj: Deploy "){
 
-        println "${app_env.DEFAULT_REPOSITORY}" +   "${DEFAULT_REPOSITORY1}" + "${IMAGE_NAME1}"
-        println "Render artefatos para ${app_env.URL_CONTAINER_REGISTRY} ${IMAGE_NAME} " 
+        println "${app_env.DEFAULT_REPOSITORY2}" +  "/" + "${DEFAULT_REPOSITORY1}" + "/" + "${IMAGE_NAME1}"
+        println "Deploy artefatos para ${app_env.URL_CONTAINER_REGISTRY} ${IMAGE_NAME} " 
           script {
                  withKubeConfig(caCertificate: '',   credentialsId: "${app_env.CREDENCIAL_K8S}", namespace: 'default', restrictKubeConfigAccess: false, serverUrl: "${app_env.CLUSTER_K8S_API}") {
                 // some block
