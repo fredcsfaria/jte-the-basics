edee0cb7caef4e7500b1dfdc0af727522c5afaa2
fredcsfaria <52935549+fredcsfaria@users.noreply.github.com>
2024-03-04 17:54:42 -0300

    Update deployk8s.groovy

--- libraries/mvcj/steps/deployk8s.groovy
+++ libraries/mvcj/steps/deployk8s.groovy
@@ -5,9 +5,9 @@ void call( app_env ){
         println "${env.REPOSITORYE}"
        // println "${app_env.DEFAULT_REPOSITORY2}" +  "/" + "${DEFAULT_REPOSITORY1}" + "/" + "${IMAGE_NAME1}"
        //def IMAGE = "${app_env.IMAGE_REPOSITORY}" +  "/" + "${REPOSITORY}" + "/" + "${IMAGE_NAME}"
-       def IMAGE = "${app_env.CONTAINER_REGISTRY}" +  "/" + "${REPOSITORY}" + "/" + "${IMAGE_NAME}" + ":" + "${IMAGE_TAG}"
+       def IMAGE = "${app_env.CONTAINER_REGISTRY}" +  "/" + "${env.REPOSITORY}" + "/" + "${env.IMAGE_NAME}" + ":" + "${env.IMAGE_TAG}"
          println(IMAGE); 
-        println "Deploy artefatos para ${app_env.URL_CONTAINER_REGISTRY} ${IMAGE_NAME} " 
+        println "Deploy artefatos para ${app_env.URL_CONTAINER_REGISTRY} ${IMAGE} " 
           script {
                  withKubeConfig(caCertificate: '',   credentialsId: "${app_env.CREDENCIAL_K8S}", namespace: 'default', restrictKubeConfigAccess: false, serverUrl: "${app_env.CLUSTER_K8S_API}") {
                 // some block
