5824708dd15ad7fc795be9ed684341f66c01878a
fredcsfaria <52935549+fredcsfaria@users.noreply.github.com>
2024-03-04 16:57:57 -0300

    Update render.groovy

--- libraries/mvcj/steps/render.groovy
+++ libraries/mvcj/steps/render.groovy
@@ -2,7 +2,7 @@ void call( app_env ){
     stage("mvcj: Render to "){
 
       //   println "${app_env.DEFAULT_REPOSITORY2}" +  "/" + "${DEFAULT_REPOSITORY1}" + "/" + "${IMAGE_NAME1}"
-        def CONTAINER_IMAGE = "${app_env.CONTAINER_REGISTRY}" +  "/" + "${IMAGE_REPOSITORY}" + "/" + "${IMAGE_NAME}"
+        def CONTAINER_IMAGE = "${app_env.CONTAINER_REGISTRY}" +  "/" + "${REPOSITORY}" + "/" + "${IMAGE_NAME}"
         println(CONTAINER_IMAGE);
         println "Render artefatos para ${app_env.URL_CONTAINER_REGISTRY} ${IMAGE_NAME} " 
            withDockerRegistry(credentialsId: 'nexus_repository', url: "${app_env.URL_CONTAINER_REGISTRY}" ) {
