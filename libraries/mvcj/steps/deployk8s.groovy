6f28e5f10c6f54e76e7dcfc663d1f6fa63f58b01
fredcsfaria <52935549+fredcsfaria@users.noreply.github.com>
2024-03-04 16:50:26 -0300

    Update deployk8s.groovy

--- libraries/mvcj/steps/deployk8s.groovy
+++ libraries/mvcj/steps/deployk8s.groovy
@@ -3,8 +3,8 @@ void call( app_env ){
 
         println "AQUI"  
         println "${SVN_REPO_URL}"
-        println "${app_env.DEFAULT_REPOSITORY2}" +  "/" + "${DEFAULT_REPOSITORY1}" + "/" + "${IMAGE_NAME1}"
-        def IMAGE = "${app_env.DEFAULT_REPOSITORY2}" +  "/" + "${DEFAULT_REPOSITORY1}" + "/" + "${IMAGE_NAME1}"
+       // println "${app_env.DEFAULT_REPOSITORY2}" +  "/" + "${DEFAULT_REPOSITORY1}" + "/" + "${IMAGE_NAME1}"
+        def IMAGE = "${app_env.IMAGE_REPOSITORY}" +  "/" + "${REPOSITORY}" + "/" + "${IMAGE_NAME}"
          println(IMAGE); 
         println "Deploy artefatos para ${app_env.URL_CONTAINER_REGISTRY} ${IMAGE_NAME} " 
           script {
