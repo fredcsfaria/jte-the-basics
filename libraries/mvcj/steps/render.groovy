29a0d8adcf320ca48a1c0b665b18b142559b8eea
fredcsfaria <52935549+fredcsfaria@users.noreply.github.com>
2024-03-04 16:48:59 -0300

    Update render.groovy

--- libraries/mvcj/steps/render.groovy
+++ libraries/mvcj/steps/render.groovy
@@ -1,14 +1,14 @@
 void call( app_env ){
     stage("mvcj: Render to "){
 
-         println "${app_env.DEFAULT_REPOSITORY2}" +  "/" + "${DEFAULT_REPOSITORY1}" + "/" + "${IMAGE_NAME1}"
-        def CONTAINER_IMAGE = "${app_env.DEFAULT_REPOSITORY2}" +  "/" + "${DEFAULT_REPOSITORY1}" + "/" + "${IMAGE_NAME1}"
+      //   println "${app_env.DEFAULT_REPOSITORY2}" +  "/" + "${DEFAULT_REPOSITORY1}" + "/" + "${IMAGE_NAME1}"
+        def CONTAINER_IMAGE = "${app_env.CONTAINER_REGISTRY}" +  "/" + "${DEFAULT_REPOSITORY}" + "/" + "${IMAGE_NAME}"
         println(CONTAINER_IMAGE);
         println "Render artefatos para ${app_env.URL_CONTAINER_REGISTRY} ${IMAGE_NAME} " 
            withDockerRegistry(credentialsId: 'nexus_repository', url: "${app_env.URL_CONTAINER_REGISTRY}" ) {
              
             //sh "skaffold render -f ./.skaffold.yaml  --digest-source=tag --default-repo=${app_env.DEFAULT_REPOSITORY}  --offline=true -p ${app_env.SKAFFOLD_PROFILE}  -o k8s-deploy.yaml -i app-image=${IMAGE_NAME} "
-            sh "skaffold render -f ./.skaffold.yaml  --digest-source=tag --default-repo=${app_env.DEFAULT_REPOSITORY}  --offline=true -p ${app_env.SKAFFOLD_PROFILE}  -o k8s-deploy.yaml -i app-image=CONTAINER_IMAGE "               
+            sh "skaffold render -f ./.skaffold.yaml  --digest-source=tag --default-repo=${app_env.IMAGE_REPOSITORY}  --offline=true -p ${app_env.SKAFFOLD_PROFILE}  -o k8s-deploy.yaml -i app-image=CONTAINER_IMAGE "               
                 }
         
     }
